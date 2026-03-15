085f395e4856a7630e656844d02eb0ba564322d1
tkitchenjr <tkitchenjr@gmail.com>
2026-03-15 17:03:10 -0500

    corrected error handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
+
 from flask import Flask, jsonify
-from werkzeug.exceptions import HTTPException
 from pydantic import ValidationError
+from app.routes.domain.response import ErrorResponse
 
 from app.db import db
 from app.routes import portfolio_bp, security_bp, trade_bp, user_bp
@@ -20,29 +21,15 @@ def create_app(config):
         app.register_blueprint(security_bp, url_prefix='/securities')
         app.register_blueprint(trade_bp, url_prefix='/trades')
 
-        # Global Exception Handler for Pydantic
-        @app.errorhandler(ValueError)
-        def handle_value_error(error):
-            db.session.rollback()  # Explicit rollback
-            return jsonify({'error': str(error)}), 400
-        
-        @app.errorhandler(HTTPException)
-        def handle_http_exception(error):
-            db.session.rollback()  # Rollback on HTTP exceptions
-            return jsonify({'error': error.description}), error.code
-
-        @app.errorhandler(Exception)
-        def handle_generic_error(error):
-            db.session.rollback()  # Rollback on any unhandled exception
-            return jsonify({'error': 'An internal error occurred'}), 500
-        
+        # Global Exception Handlers for Pydantic
         @app.errorhandler(ValidationError)
         def handle_validation_error(error):
-            # Extract first error message for simplicity
             first_error = error.errors()[0]
             error_message = f"{first_error['loc'][0]}: {first_error['msg']}"
-            return jsonify({'error': error_message, 'code': 400}), 400
+            return jsonify(ErrorResponse(error='Validation Error', detail=error_message).model_dump()), 422
+        
         return app
+    
     except Exception as e:
         print(f'Error creating app: {e}')
         raise
