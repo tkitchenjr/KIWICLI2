fccd96d0231d3ede4ffb157ef747ad2af126f7bb
tkitchenjr <tkitchenjr@gmail.com>
2026-03-23 11:09:01 -0500

    Clean Cache and gitignore

--- app/service/alpha_vantage_client.py
+++ app/service/alpha_vantage_client.py
@@ -1,6 +1,11 @@
 from app.config import Config
 from dataclasses import dataclass
 import requests
+from flask_caching import SimpleCache
+
+# Initialize Cache variable
+cache = SimpleCache(default_timeout=300) 
+
 
 #Alpha Vantage Error Handling
 class AlphaVantageError(Exception):
@@ -31,6 +36,14 @@ def get_api_key():
 # Queries the Alpha Vantage API and returns the issuer name associated with the given ticker symbol. 
 # Returns None if the ticker is not found.
 def get_company_name(ticker: str):
+
+    # Check Cache 
+    key = f'company_name_{ticker}'
+    cached_name = cache.get(key)
+    if cached_name is not None:
+        return cached_name
+    
+    # Defer to API if not in cache
     api_key = get_api_key()
     url = f'https://www.alphavantage.co/query?function=OVERVIEW&symbol={ticker}&apikey={api_key}'
 
@@ -42,13 +55,22 @@ def get_company_name(ticker: str):
         raise AlphaVantageError('No matching ticker found.')
     if not "Name" in data:
         return None
+    
+    # Cache the result before returning
+    cache.set(key, data.get("Name"))
     return data.get("Name")
 
 # get_price_data(ticker: str) -> dict | None — 
 # Retrieves the most recent available price data for a given ticker. 
 # Returns a dictionary with price fields (e.g., open, high, low, close, volume). 
 # Returns None if data is unavailable.
-def get_price_data(ticker: str):
+def get_price_data(ticker: str) -> dict | None:
+    # Check Cache
+    key = f'price_data_{ticker.upper()}'
+    cached_data = cache.get(key)
+    if cached_data is not None:
+        return cached_data
+
     api_key = get_api_key()
     url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={ticker}&interval=5min&apikey={api_key}'
 
@@ -63,14 +85,14 @@ def get_price_data(ticker: str):
     latest_timestamp = max(time_series.keys())
     data = time_series[latest_timestamp]
 
-    return {
+    result = {
         'date': latest_timestamp,
-        'open': float(data["1. open"]),
-        'high': float(data["2. high"]),
-        'low': float(data["3. low"]),
         'close': float(data["4. close"]),
-        'volume': int(data["5. volume"])
+        
     }
+    # Cache the result before returning
+    cache.set(key, result)
+    return result
 
 # get_quote(ticker: str) -> SecurityQuote | None 
 # A convenience function that calls get_company_name and get_price_data internally
